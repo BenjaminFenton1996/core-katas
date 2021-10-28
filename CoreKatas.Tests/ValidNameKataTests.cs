917e625817960e3edf717d01804a73cc941cc55c
Benny Fenton <thesupereggofdoom@googlemail.com>
2021-10-28 13:42:50 +0100

    Completed Valid Name kata

---
+++ CoreKatas.Tests/ValidNameKataTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreKatas.Tests
+{
+    class ValidNameKataTests
+    {
+        //Names must have a length of 4 or more
+        //Names must start with a letter
+        //Names must consist of only letters, numbers and optionally one underscore
+        //Names must not end with an underscore
+
+        [Test]
+        public void TestNameLength()
+        {
+            Assert.IsTrue(ValidNameKata.IsNameValid("Steve"));
+            Assert.IsFalse(ValidNameKata.IsNameValid("Bob"));
+        }
+
+        [Test]
+        public void TestNameHasValidUnderscores()
+        {
+            Assert.IsTrue(ValidNameKata.IsNameValid("Steve_Guy"));
+            Assert.IsFalse(ValidNameKata.IsNameValid("Steve__Guy"));
+            Assert.IsFalse(ValidNameKata.IsNameValid("SteveGuy_"));
+        }
+
+        [Test]
+        public void TestNameHasValidCharacters()
+        {
+            Assert.IsTrue(ValidNameKata.IsNameValid("Steve_Guy"));
+            Assert.IsTrue(ValidNameKata.IsNameValid("Steve_Guy1"));
+            Assert.IsFalse(ValidNameKata.IsNameValid("Steve-Guy"));
+        }
+
+        [Test]
+        public void TestNameStartsWithLetter()
+        {
+            Assert.IsTrue(ValidNameKata.IsNameValid("Steve_Guy"));
+            Assert.IsFalse(ValidNameKata.IsNameValid("1Steve_Guy"));
+            Assert.IsFalse(ValidNameKata.IsNameValid("_Steve_Guy"));
+        }
+    }
+}
---
+++ CoreKatas/ValidNameKata.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CoreKatas
+{
+    public static class ValidNameKata
+    {
+        public static bool IsNameValid(string name)
+        {
+            if (name.Length < 4)
+            {
+                return false;
+            }
+            else if (name.EndsWith("_"))
+            {
+                return false;
+            }
+            else if (!char.IsLetter(name.FirstOrDefault()))
+            {
+                return false;
+            }
+            else if (name.Where(x => x == '_').Count() > 1)
+            {
+                return false;
+            }
+            else if (!Regex.IsMatch(name, @"^[a-zA-Z0-9_]+$"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
